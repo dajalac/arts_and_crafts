0bcd2f69a69632cf0fb0efb3be08cc16cdbd4e7b
danielle ajala <danielleajala@gmail.com>
2021-05-15 14:57:19 -0500

    navbar moving

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+/*let hamburgerMenu = document.querySelector(".hamburger-menu");
+let container = document.querySelector(".container");
+
+hamburgerMenu.addEventListener("click",()=>{
+    container.classList.toggle("change");
+});*/
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+  });
+
+
+
+
