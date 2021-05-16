43779586c60e5910679b94637845fcd876ce66a8
danielle ajala <danielleajala@gmail.com>
2021-05-15 19:57:48 -0500

    contact, footer and @media

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 
 
 
-/*let hamburgerMenu = document.querySelector(".hamburger-menu");
+
+let hamburgerMenu = document.querySelector(".hamburger-menu");
 let container = document.querySelector(".container");
+
+
 
 hamburgerMenu.addEventListener("click",()=>{
     container.classList.toggle("change");
-});*/
+});
 
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
-  });
 
+
+
+ 
+
+
+
+ 
 
 
 
